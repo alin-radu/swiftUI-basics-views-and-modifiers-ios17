1e63191af3b3dd9de63cfdfc8199d960be3b94b0
Alin RADU <alin.radu.cristian@gmail.com>
2024-02-05 20:50:21 +0200

    Initial Commit

---
+++ ViewsAndModifiers-ios17/ViewsAndModifiers_ios17App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiers_ios17App.swift
+//  ViewsAndModifiers-ios17
+//
+//  Created by Alin RADU on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiers_ios17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
